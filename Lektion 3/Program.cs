aa7763ae7862adb9dd93e04285bec368d7d7402d
Haddy Sosseh <Haddy.Sosseh@elev.ga.ntig.se>
2023-10-17 13:37:53 +0200

    Lucktext

---
+++ Lektion 3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hi, what's your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Wow," + name + ". What an ugly name! anyways, how's your day going?");
+
+string goodorbad = Console.ReadLine();
+
+Console.WriteLine("...");
+
+string attention = Console.ReadLine();
+
+Console.WriteLine("Huh? oh, sorry i wasnt really paying attention. It's good your day was " + goodorbad + " or whatever. But hey, can you do me a favor?");
+
+string favor = Console.ReadLine();
+
+Console.WriteLine(favor +"? Yeah, i dont really care what your answer was. Anyways, i wanted you to close the program cause I'm getting a bit tired of having to look at your face. I didn't want to say it but you're a bit of an eye sore..");
+
+Console.ReadLine(); 
